22a05e85554e38ccb22194fcc76e8cdc8972cf26
xyu <1357525676@qq.com>
2021-02-24 15:26:46 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+  }
+
+  .el-form--inline .el-form-item {
+    margin-right: 0 !important;
+  }
+</style>
